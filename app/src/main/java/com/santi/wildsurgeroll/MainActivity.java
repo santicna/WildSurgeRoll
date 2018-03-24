ce33fef1ce44607fc8f205a747118dcc13a9fac5
santiago cartagena <scartagena147@gmail.com>
2018-03-24 12:08:54 -0300

    Add onClickListener Method to Roll Button

--- app/src/main/java/com/santi/wildsurgeroll/MainActivity.java
+++ app/src/main/java/com/santi/wildsurgeroll/MainActivity.java
@@ -2,6 +2,7 @@ package com.santi.wildsurgeroll;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -25,6 +26,27 @@ public class MainActivity extends AppCompatActivity {
         mRollEffect = findViewById(R.id.roll_effect);
         mRollButton = findViewById(R.id.roll_button);
         mResetButton = findViewById(R.id.reset_button);
+
+
+        mRollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Hide the roll Button
+                mRollButton.setVisibility(View.INVISIBLE);
+                //Roll de the dice
+                int result = Roll.rollDice();
+                //Get the effect for the number rolled
+                String effect = Roll.getEffect(result);
+                //Show the number rolled
+                mRollResult.setText(String.valueOf(result));
+                mRollResult.setVisibility(View.VISIBLE);
+                //Show the effect rolled
+                mRollEffect.setText(effect);
+                mRollEffect.setVisibility(View.VISIBLE);
+                //Show reset button
+                mResetButton.setVisibility(View.VISIBLE);
+            }
+        });
 
     }
 
